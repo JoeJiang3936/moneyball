f5e08499fb0893beb5e31fb4748930e8f168ad36
Joe Jiang <yanliu3936@gmail.com>
2018-09-19 16:06:27 -0500

    add lm R vs HR + BB

---
+++ lm_R vs HR and BB.R
@@ -0,0 +1,13 @@
+library(Lahman)
+library(dplyr)
+library(ggplot2)
+
+#calculates the coefficiency for predicting runs per game based on HR per game and 
+#BB per game
+
+
+Galton_heights <- Teams%>%filter(yearID %in% 1961:2001)%>%
+  mutate(BB_per_game = BB/G, HR_per_game = HR/G, R_per_game = R/G)
+
+fit_R_HR_BB <- lm(R_per_game~HR_per_game+BB_per_game, Galton_heights)
+fit_R_HR_BB
